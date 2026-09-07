adf9df48b01180a23d613799186420cf14070c5a
jsuryanaads <jsuryanaads@gmail.com>
2026-09-07 19:29:53 +0700

    refactor: keep layout dependency free

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,3 @@
-import { Outlet } from 'react-router-dom';
-
-export default function MainLayout() {
-  return (
-    <div className="app-shell">
-      <Outlet />
-    </div>
-  );
+export default function MainLayout({ children }) {
+  return <div className="app-shell">{children}</div>;
 }
