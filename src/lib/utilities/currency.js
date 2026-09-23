2843558c6edbfec901dbc997545f4f03d1e1227e
We DESIGN <jsuryanaads@gmail.com>
2026-09-23 07:38:11 +0700

    chore: remove unused legacy file src/lib/utilities/currency.js

--- src/lib/utilities/currency.js
+++
@@ -1,8 +0,0 @@
-export const rupiah = new Intl.NumberFormat('id-ID', {
-  style: 'currency',
-  currency: 'IDR',
-  maximumFractionDigits: 0,
-});
-
-export const formatRupiah = (value) => rupiah.format(Number(value) || 0);
-export const formatCurrency = formatRupiah;
