1a6bbb68ab1f4dfecf82b2dc8d35d9bdaf6c94c9
We DESIGN <jsuryanaads@gmail.com>
2026-09-23 07:37:45 +0700

    chore: remove unused legacy file src/components/navigation/NavItem.jsx

--- src/components/navigation/NavItem.jsx
+++
@@ -1,8 +0,0 @@
-export default function NavItem({ icon: Icon, label, active = false, onClick }) {
-  return (
-    <button className={`nav-item ${active ? 'active' : ''}`} onClick={onClick}>
-      {Icon && <Icon size={19} />}
-      <span>{label}</span>
-    </button>
-  );
-}
